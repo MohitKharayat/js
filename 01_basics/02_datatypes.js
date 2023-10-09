41574c659342bf09a310e6c54a009165290ba495
Mohit Kharayat <85476505+MohitKharayat@users.noreply.github.com>
2023-10-09 17:10:54 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strict"; // treat all  js code as newer version
 
-let name = "mohit" //string
+let name = "mohit" //string => ""
 let age = 69 //number => 2 to power 53
 let isLoggedIn = false //boolean => true/false
 // symbol => unique
