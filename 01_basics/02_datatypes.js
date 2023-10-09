852dc852524b186db554c5814bf90bb67f6bc7ba
Mohit Kharayat <85476505+MohitKharayat@users.noreply.github.com>
2023-10-09 11:38:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all  js code as newer version
+
+let name = "mohit" //string
+let age = 69 //number => 20 to power 53
+let isLoggedIn = false //boolean => true/false
+// symbol => unique
+// bigint
+// null => standalone value
+
+// object
+
+console.log(typeof age); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
