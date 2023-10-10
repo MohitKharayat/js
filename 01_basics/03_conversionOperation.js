91afa9c887148eb604fd918a89047255824d2203
Mohit Kharayat <85476505+MohitKharayat@users.noreply.github.com>
2023-10-10 06:35:20 +0000

    Operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "45xyz"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); //NaN
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); //NaN
 
 //number "33" => 33
 //"123sdf" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber); //NaN
 
 let isLoggedIn = "mk"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 //true => 1; false => 0
 //"" => false
@@ -23,5 +23,35 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 34
 let stringNumber = String(someNumber)
-console.log(stringNumber)  //"34"
-console.log(typeof stringNumber)  //string 
+//console.log(stringNumber)  //"34"
+//console.log(typeof stringNumber)  //string 
+
+/* ******************Operations************* */
+let value = 3
+let negValue = -value
+//console.log(negValue) //-3
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hi"
+let str2 = " Mohit"
+let str3 = str1+str2
+// console.log(str3) //Hi Mohit
+
+// console.log("2"+2) //22
+// console.log(2+"2") //22
+// console.log("2"+"2") //22
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+
+// console.log(+true) //1
+// console.log(+"") //0
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
